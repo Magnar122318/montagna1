deb6ef3d374b67e3725a4e1ce0e61cca125503b4
Sandro Gustavo Guerrero Vilela <56529935+Magnar122318@users.noreply.github.com>
2021-12-29 16:30:57 -0500

    host

--- web/settings/production.py
+++ web/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['montagnadeveloper.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
